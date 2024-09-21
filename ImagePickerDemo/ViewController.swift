a76cc9ece053a5003b9c37d94c75027726bb4a5f
Oscar Alvarado <alvaradoo@uamv.edu.ni>
2024-09-21 14:39:53 -0600

    Initial Commit

---
+++ ImagePickerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagePickerDemo
+//
+//  Created by Oscar Alvarado on 21/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
